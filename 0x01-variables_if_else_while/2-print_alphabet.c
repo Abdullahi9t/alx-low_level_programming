de4e79b8e4cb13df278490a01ee5525128b08ddf
Abdullahi9t <Ahulfadhaty333@gmail.com>
2023-02-17 21:40:20 -0800

    1-last_digit.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * writing a program that print alphabet in lower case
+ *follow by a new lin
+ */
+ int main()
+{
+	char alp;
+	for (alp = 'a'; alp <= 'z'; alp++)
+		putchar(alp);
+
+	putchar('\n');
+
+	return 0;
+}
