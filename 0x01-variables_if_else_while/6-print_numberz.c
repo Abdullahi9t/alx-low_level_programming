e942df4bed37601cb5b87355fedc2decb4bf2192
Abdullahi9t <Ahulfadhaty333@gmail.com>
2023-02-19 15:53:32 -0800

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main _ main block
+ * main -  main block
  * Declaration: Prints all single digit number of base 10
  * starting from 0, followed by new line.
  * Return: Always 0
