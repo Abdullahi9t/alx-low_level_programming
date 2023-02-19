80b3fe9b65e6ba5d7399b141f9edf56df472f03b
Abdullahi9t <Ahulfadhaty333@gmail.com>
2023-02-19 15:45:10 -0800

    7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - main block
- * Declaration: prints all digit number of base 10 
+ * Declaration: prints all digit number of base 10
  * starting from 0,followed by a new line
  * Return: Always 0
  */
@@ -14,7 +14,6 @@ int main(void)
 		putchar(c);
 		c--;
 	}
-        
         putchar('\n');
 	return (0);
 }
