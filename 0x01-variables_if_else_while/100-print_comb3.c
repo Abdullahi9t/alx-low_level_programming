653a6b581a5ebbd14d2af610d611ad9406cb492c
Abdullahi9t <Ahulfadhaty333@gmail.com>
2023-02-19 16:59:45 -0800

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - main block
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i = 01;
+
+	while (i <= 89)
+	{
+		putchar(i + '01');
+		if (i == 89)
+			break;
+		putchar(',');
+		putchar(' ');
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
