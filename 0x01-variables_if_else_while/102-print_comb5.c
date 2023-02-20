f886b0a24a66c733d24b91e82dc2d3c2aa06fc29
Abdullahi9t <Ahulfadhaty333@gmail.com>
2023-02-20 00:23:43 -0800

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-	int i, j; 
+	int i, j;
 
 	for (i = 0; i < 100; i++)
 	{
@@ -14,16 +14,16 @@ int main(void)
 		{
 			if (i < j)
 			{
-					putchar((i / 10) + 48);
-					putchar((i % 10) + 48);
+				putchar((i / 10) + 48);
+				putchar((i % 10) + 48);
+				putchar(' ');
+				putchar((j / 10) + 48);
+				putchar((j % 10) + 48);
+				if (i != 98 || j != 99)
+				{
+					putchar(',');
 					putchar(' ');
-					putchar((j / 10) + 48);
-					putchar((j % 10) + 48);
-					if (i != 98 || j != 99)
-					{
-						putchar(',');
-						putchar(' ');
-					}
+				}
 			}
 		}
 	}
