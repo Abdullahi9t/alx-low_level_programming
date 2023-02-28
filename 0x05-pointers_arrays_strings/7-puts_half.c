94704e548dd27005c09b85939c670c7485cf7ff8
Abdullahi9t <Ahulfadhaty333@gmail.com>
2023-02-28 05:51:44 -0800

    arrarys

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,7 +11,7 @@ void puts_half(char *str)
 	
 	len = 0;
 
-	while (str[len] != '\0');
+	while (str[len] != '\0')
 	{
 		len++;
 	}
@@ -25,7 +25,7 @@ void puts_half(char *str)
 	}
 	else if (len % 2)
 	{
-		for (n = (len - 1) / 2; n < len - 1, i++)
+		for (n = (len - 1) / 2; n < len - 1; i++)
 		{
 			putchar(str[n + 1]);
 		}
